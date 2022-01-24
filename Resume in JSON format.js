e8398ae7a8ca90146588333243f40134cd1d14c8
karthichillz <97529707+karthichillz@users.noreply.github.com>
2022-01-24 14:09:12 +0530

    Add files via upload

---
+++ Resume in JSON format.js
@@ -0,0 +1,39 @@
+{
+    "basics": {
+      "name": "Karthik M",
+      "label": "Programmer",
+      "email": "karthikraja@gmail.com",
+      "phone": "9361203364",
+      "location": {
+        "address": "Kamaraj street",
+        "postalCode": "638 183",
+        "city": "Namakkkal",
+        "countryCode": "IND",
+        "region": "Tamil Nadu"
+      }
+    },
+    "education": [{
+      "institution": "AMET University",
+      "area": "Naval architecture",
+      "studyType": "Bachelor",
+      "startYear": "2014",
+      "endYear": "2021",
+      "gpa": "6.0",
+      "courses": [
+        "DB1101 - Basic python"
+      ]
+    }],
+    "projects": [{
+              "project_name": "preliminary design of container ship 4250TEU",
+              "description": [
+                  "This Project is about the design of Container ship",
+                  "that deadweight of 51693T  (4250 TEU) and the length of 249.4 m depth of 18.8m and the draft of 13.3m."
+              ]
+          }],
+    "skills": [{
+      "name": "full stack Development",
+      "level": "Beginner",
+      "keywords": ["HTML","CSS","Javascript","python"]
+    }]
+  }
+  
